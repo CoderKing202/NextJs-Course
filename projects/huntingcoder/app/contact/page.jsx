e7c8f9e83ccfa17a73694c0aab9153c8ae35a9cb
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2026-02-23 16:05:48 +0530

    Lesson 26 Designing the /contact FrontEnd Page

--- projects/huntingcoder/app/contact/page.jsx
+++ projects/huntingcoder/app/contact/page.jsx
@@ -1,10 +1,110 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import styles from "@/styles/contact.module.css";
+
 // import "../styles.css"
 function page() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [desc, setDescription] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, phone, desc);
+    const data = { name, email, phone, desc };
+    fetch("http://localhost:3000/api/postcontact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success", data);
+        alert("Thanks for contacting us")
+        setName("")
+        setEmail("")
+        setDescription("")
+        setPhone("")
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  const handleChange = (e) => {
+    if (e.target.name == "phone") {
+      setPhone(e.target.value);
+    } else if (e.target.name == "email") {
+      setEmail(e.target.value);
+    } else if (e.target.name == "desc") {
+      setDescription(e.target.value);
+    } else if (e.target.name == "name") {
+      setName(e.target.value);
+    }
+  };
   return (
-    <div>This is contact page</div>
-  )
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.mb3}>
+          <label htmlFor="name" className={styles.formlabel}>
+            Enter your name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            aria-describedby="emailHelp"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="email" className={styles.formlabel}>
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="phone" className={styles.formlabel}>
+            Password
+          </label>
+          <input
+            type="phone"
+            name="phone"
+            value={phone}
+            className="form-control"
+            id="phone"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles.mb3}>
+          <label htmlFor="floatingTextarea">Elaborate your concern</label>
+          <textarea
+            className="form-control"
+            placeholder="Write your concern here"
+            id="desc"
+            name="desc"
+            value={desc}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default page
+export default page;
