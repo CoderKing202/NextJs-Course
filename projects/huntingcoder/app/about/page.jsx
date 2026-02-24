3a900b0818979d959d14c423988ed16d785fe2cd
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2026-02-24 14:47:02 +0530

    Lesson 27 Designing the About Page

--- projects/huntingcoder/app/about/page.jsx
+++ projects/huntingcoder/app/about/page.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import styles from "@/styles/about.module.css"
 function page() {
   return (
-    <div>This is my about component</div>
+    <div className={styles.container}>
+      <h1 className={styles.center}>About Hunting Coder</h1>
+      <h2>Introduction</h2>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto eum beatae iste excepturi reprehenderit, reiciendis in, nemo culpa alias sunt est eius. Ullam quidem architecto omnis consequatur voluptates distinctio, laudantium, sunt suscipit sapiente nostrum debitis iusto tenetur vel eum recusandae assumenda similique adipisci laboriosam nihil hic accusantium illo quam blanditiis rerum. Ipsam modi porro obcaecati assumenda labore? Sequi illum provident natus tenetur quae atque recusandae eos? Voluptas autem expedita porro! Amet earum laborum ex aspernatur magni minus. Magnam optio ipsum nam. Voluptates earum suscipit exercitationem aliquid officia, quae incidunt vitae fuga, architecto ipsa qui accusantium autem adipisci repellendus voluptas, nesciunt recusandae soluta ullam! Autem, cumque doloribus ad voluptatibus quos asperiores. Dolores quaerat facere quia neque officia, deleniti odio corrupti maiores alias, impedit iusto pariatur? Dicta exercitationem qui adipisci ab tempora illo necessitatibus veritatis magnam nihil omnis hic, minima dolore dolor alias esse ipsam sint eveniet culpa molestias atque quo. Cupiditate ipsam laborum dolorem mollitia tenetur unde excepturi veniam. Placeat et non, magni minus similique natus molestias expedita odit dicta modi fugit accusantium voluptates doloribus animi fuga. Esse, itaqu
+    <h2>Service Offered</h2>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non porro quam quae iste adipisci corporis officia. Possimus, cupiditate soluta repellendus odio ullam, mollitia vel deserunt dignissimos ipsum esse accusamus illum perferendis modi dicta distinctio!
+    <p>We offer the following services</p>
+    <ul>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+      <li>Service 1</li>
+    </ul>
+    <h2>Contact Us</h2>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam dolores molestias neque consequuntur eos, veniam delectus veritatis, reiciendis sequi odit, sint accusamus eligendi officiis facilis maiores. Laboriosam nulla consectetur itaque numquam ad facilis molestiae temporibus sit totam, unde obcaecati, asperiores blanditiis ut, architecto magnam. Cum unde sint assumenda doloremque doloribus voluptates, eum rem aliquid.
+    </div>
   )
 }
 
