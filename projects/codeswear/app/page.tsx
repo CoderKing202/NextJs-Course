5401fab3f8ab0ec0812a4d0859203f1f12bfcc72
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2026-03-12 14:31:46 +0530

    Creating Pages for Codeswear.com

--- projects/codeswear/app/page.tsx
+++ projects/codeswear/app/page.tsx
@@ -4,16 +4,10 @@ export default function Home() {
   return (
     <div>
       Hey this is codeswear
-      <div className="mx-4">
-        This is me
-      </div>
-      <div className="mx-4 bg-slate-500">
-        This is me
-      </div>
-      <div className="bg-green-800 mx-60">
-        Hey bro
-      </div> 
-
+      <div className="mx-4">This is me</div>
+      <div className="mx-4 bg-slate-500">This is me</div>
+      <div className="bg-green-800 mx-60">Hey bro</div>
     </div>
   );
 }
+  
---
+++ projects/codeswear/app/product/[slug]/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+import {useParams} from "next/navigation"
+const page = () => {
+    const params = useParams()
+  return (
+    <div>
+        Slug is {params.slug}        
+    </div>
+  )
+}
+
+export default page
