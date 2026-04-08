a2e68e205ac75f6684370adeb39e52f4ee47defd
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2026-04-08 10:41:45 +0530

    Lesson 44 Creating Orders Page for Codeswear.com(real)

--- projects/codeswear/app/orders/page.jsx
+++ projects/codeswear/app/orders/page.jsx
@@ -32,7 +32,8 @@ function page() {
         <div className="flex flex-col">
           <span className="title-font font-medium text-2xl text-gray-900">SubTotal: ₹1158.00</span>
            <div className='my-6'>
-          <button className="flex mx-0 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded">Track Order</button>
+          <button className="flex mx-0 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-non
+          e hover:bg-pink-600 rounded">Track Order</button>
           </div>
         </div>
       </div>
