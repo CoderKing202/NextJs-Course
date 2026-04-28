abb255a7ee6c6fe9659debf22a2242041a8c9f51
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2026-04-28 07:01:00 +0530

    Lesson 49 Displaying Tshirt Variants on product page

--- projects/codeswear/app/api/getproducts/route.jsx
+++ projects/codeswear/app/api/getproducts/route.jsx
@@ -1,7 +1,31 @@
-import Product from "@/models/Product"
-import connectDb from "../../../helper/mongoose"
-export  async function GET(){
-    await connectDb()
-    let products = await Product.find()
-    return Response.json({products})    
+import Product from "@/models/Product";
+import connectDb from "../../../helper/mongoose";
+export async function GET() {
+  await connectDb();
+  let products = await Product.find();
+  let tshirts = {};
+  for (let item of products) {
+    if (item.title in tshirts) {
+      if (
+        !tshirts[item.title].color.includes(item.color) &&
+        item.availableQty > 0
+      ) {
+        tshirts[item.title].color.push(item.color);
+      }
+      if (
+        !tshirts[item.title].size.includes(item.size) &&
+        item.availableQty > 0
+      ) {
+        tshirts[item.title].size.push(item.size);
+      }
+    } else {
+      tshirts[item.title] = JSON.parse(JSON.stringify(item));
+      if (item.availableQty > 0) {
+        tshirts[item.title].color = [item.color];
+        tshirts[item.title].size = [item.size];
+      }
+    }
+  }
+  
+  return Response.json({ tshirts });
 }
